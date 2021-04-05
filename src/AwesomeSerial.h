a4b72c92c9a9efd6c0bdfad63e2d256ba113b0d4
Potewo <potewo@protonmail.com>
2021-04-06 07:57:16 +0900

    initial commit

---
+++ src/AwesomeSerial.h
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+class AwesomeSerial {
+  public:
+    int bufSize;
+    uint8_t* buf;
+    void init(int bufSize, unsigned long baud);
+    void send(const char* data);
+    int receive();
+};
